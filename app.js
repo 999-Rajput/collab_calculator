45661f3441fbe09ee1fcda8adf8c37b9c2123553
Hetal Kumar <70019421+999-Rajput@users.noreply.github.com>
2023-09-25 03:01:57 +0000

    create a window

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.warn(' Hy mediveda')
+const { app, BrowserWindow } = require('electron')
+function createWindow () {
+    let mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        
+        webPreferences: {
+          nodeIntegration: true
+        }
+      });
+    mainWindow.loadFile('index.html');
+    mainWindow.setMenuBarVisibility(false);
+    mainWindow.setResizable(false);
+}
+app.whenReady().then(createWindow);
